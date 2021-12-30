59731371df947ca11cf58ddcb240f51624ff3cb9
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2021-12-30 17:36:58 -0500

    About page updated, gatsby-plugin-image added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
     return (
         <Layout>
-            Hello from AboutPage
+            <main className='page'>
+                <section className='about-page'>
+                    <article>
+                        <h2>Gastby Recipes</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, reiciendis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, adipisci! Aliquid qui mollitia sunt natus?</p>
+                        <Link to='/contact' className='btn'>contact</Link>
+                    </article>
+                    <StaticImage src='../assets/images/about.jpeg' alt='About Image' className='about-img' placeholder='blurred'/>
+                </section>
+            </main>
         </Layout>
     )
 }
